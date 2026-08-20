6e5d44a9d4f3091730e2fef18a102035cb09d127
GeorgeFatan <127293166+GeorgeFatan@users.noreply.github.com>
2026-08-20 21:42:57 +0300

    Update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { GameModule } from './game/game.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [AuthModule, UsersModule, GameModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
