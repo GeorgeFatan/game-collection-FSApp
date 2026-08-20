daf1bb142634260c6f2d21d5ac78e895e0c77c29
GeorgeFatan <127293166+GeorgeFatan@users.noreply.github.com>
2026-08-20 22:38:32 +0300

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
     origin: [
       'http://localhost:5173',
       'https://game-collection-fs-app.vercel.app',
+      /\.vercel\.app$/,
     ],
     credentials: true,
   });
