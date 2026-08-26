dc4de41dac11117d8f32cb439f688808a5ad4cbf
GeorgeFatan <127293166+GeorgeFatan@users.noreply.github.com>
2026-08-26 15:00:52 +0300

    take 2

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,12 +5,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: true,
+    origin: 'https://game-collection-fs-app.vercel.app',
     methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
 
-  await app.listen(process.env.PORT || 8080);
+  await app.listen(process.env.PORT || 8080, '0.0.0.0');
 }
+
 bootstrap();
